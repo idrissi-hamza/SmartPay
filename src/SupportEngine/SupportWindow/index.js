a38a16dbe50a7cdcc398fd3e6c927ce2b9fab3bf
A-IdrissiHamza <a.idrissihamza@gmail.com>
2023-03-12 19:38:58 +0100

    create chat api

--- src/SupportEngine/SupportWindow/index.js
+++ src/SupportEngine/SupportWindow/index.js
@@ -1,21 +1,28 @@
-import React from "react";
+import React, { useState } from 'react';
 
-import { styles } from "../styles";
-import EmailForm from "./EmailForm";
+import { styles } from '../styles';
+import EmailForm from './EmailForm';
 
+const SupportWindow = (props) => {
+  const [user, setUser] = useState(null);
+  const [chat, setChat] = useState(null);
+  return (
+    <div
+      className="transition-5"
+      style={{
+        ...styles.supportWindow,
+        ...{ opacity: props.visible ? '1' : '0' },
+      }}
+    >
+      <EmailForm
+        visible={user === null || chat === null}
+        setUser={(user) => setUser(user)}
+        setChat={(chat) => setChat(chat)}
+      />
 
-const SupportWindow = props => {
-    return (
-        <div 
-            className='transition-5'
-            style={{
-                ...styles.supportWindow,
-                ...{ opacity: props.visible ? '1' : '0' }
-            }}
-        >
-          <EmailForm />
-        </div>
-    )
-}
+      {user !== null && chat !== null && <div>Chat Engine</div>}
+    </div>
+  );
+};
 
 export default SupportWindow;
