5981d561f015334a70c9cfc6ba59281f6d1a2664
A-IdrissiHamza <a.idrissihamza@gmail.com>
2023-03-12 16:46:38 +0100

    adding features page

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -39,6 +39,214 @@ const LandingPage = () => {
           </div>
         </div>
       </section>
+      <section class="dorik-section dorik-section-f5kyae5m ">
+        <div class="container">
+          <div class="dorik-row-591s8oid  flex">
+            <div class="col-lg-1/1 ">
+              <div class="column-inner dorik-column-r3ivv2hb  1/1">
+                <div class="dorik-wrapper dorik-heading-q1u5v0km-wrapper ">
+                  <h6
+                    class="dorik-heading dorik-heading-q1u5v0km "
+                    data-aos-easing="ease"
+                    data-aos-duration="600"
+                  >
+                    <span>Why Choose Dpay</span>
+                  </h6>
+                </div>
+                <div class="dorik-wrapper dorik-heading-wq3ztfa6-wrapper ">
+                  <h2
+                    class="dorik-heading dorik-heading-wq3ztfa6 "
+                    data-aos-easing="ease"
+                    data-aos-duration="600"
+                    data-aos-delay="100"
+                  >
+                    <span>Secure & Seamless Payment</span>
+                  </h2>
+                </div>
+                <div
+                  class="dorik-text dorik-text-43etyhlv "
+                  data-aos-easing="ease"
+                  data-aos-duration="600"
+                  data-aos-delay="100"
+                >
+                  Funding branding user experience business-to-business
+                  bandwidth social proof startup value proposition A/B testing
+                  focus supply chain crowdfunding.
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="container">
+          <div class="dorik-row-20hlfpdy  flex">
+            <div class="col-lg-1/3 ">
+              <div class="column-inner dorik-column-yvpaamen  1/3">
+                <div class="dorik-icon-9v3zhyq1-wrapper  ">
+                  <span class="dorik-icon dorik-icon-9v3zhyq1 ">
+                    <svg
+                      aria-hidden="true"
+                      focusable="false"
+                      data-prefix="far"
+                      data-icon="credit-card"
+                      class="svg-inline--fa fa-credit-card fa-w-18 fa-fw icon"
+                      role="img"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 576 512"
+                    >
+                      <path
+                        fill="currentColor"
+                        d="M527.9 32H48.1C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48.1 48h479.8c26.6 0 48.1-21.5 48.1-48V80c0-26.5-21.5-48-48.1-48zM54.1 80h467.8c3.3 0 6 2.7 6 6v42H48.1V86c0-3.3 2.7-6 6-6zm467.8 352H54.1c-3.3 0-6-2.7-6-6V256h479.8v170c0 3.3-2.7 6-6 6zM192 332v40c0 6.6-5.4 12-12 12h-72c-6.6 0-12-5.4-12-12v-40c0-6.6 5.4-12 12-12h72c6.6 0 12 5.4 12 12zm192 0v40c0 6.6-5.4 12-12 12H236c-6.6 0-12-5.4-12-12v-40c0-6.6 5.4-12 12-12h136c6.6 0 12 5.4 12 12z"
+                      ></path>
+                    </svg>
+                  </span>
+                </div>
+                <div class="dorik-wrapper dorik-heading-vnpfnszn-wrapper ">
+                  <h3 class="dorik-heading dorik-heading-vnpfnszn ">
+                    <span>Secure Payment</span>
+                  </h3>
+                </div>
+                <div class="dorik-text dorik-text-ye99tsn0 ">
+                  Ecosystem bootstrapping learning curve lean startup
+                  disruptive. Marketing long tail disruptive agile development
+                  partner.
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-1/3 ">
+              <div class="column-inner dorik-column-4rv6ivje  1/3">
+                <div class="dorik-icon-eb99cq33-wrapper  ">
+                  <span class="dorik-icon dorik-icon-eb99cq33 ">
+                    <svg
+                      aria-hidden="true"
+                      focusable="false"
+                      data-prefix="far"
+                      data-icon="compass"
+                      class="svg-inline--fa fa-compass fa-w-16 fa-fw icon"
+                      role="img"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 496 512"
+                    >
+                      <path
+                        fill="currentColor"
+                        d="M347.94 129.86L203.6 195.83a31.938 31.938 0 0 0-15.77 15.77l-65.97 144.34c-7.61 16.65 9.54 33.81 26.2 26.2l144.34-65.97a31.938 31.938 0 0 0 15.77-15.77l65.97-144.34c7.61-16.66-9.54-33.81-26.2-26.2zm-77.36 148.72c-12.47 12.47-32.69 12.47-45.16 0-12.47-12.47-12.47-32.69 0-45.16 12.47-12.47 32.69-12.47 45.16 0 12.47 12.47 12.47 32.69 0 45.16zM248 8C111.03 8 0 119.03 0 256s111.03 248 248 248 248-111.03 248-248S384.97 8 248 8zm0 448c-110.28 0-200-89.72-200-200S137.72 56 248 56s200 89.72 200 200-89.72 200-200 200z"
+                      ></path>
+                    </svg>
+                  </span>
+                </div>
+                <div class="dorik-wrapper dorik-heading-basnuak9-wrapper ">
+                  <h3 class="dorik-heading dorik-heading-basnuak9 ">
+                    <span>Secure Payment</span>
+                  </h3>
+                </div>
+                <div class="dorik-text dorik-text-tpijx62p ">
+                  Ecosystem bootstrapping learning curve lean startup
+                  disruptive. Marketing long tail disruptive agile development
+                  partner.
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-1/3 ">
+              <div class="column-inner dorik-column-nixkiu4x  1/3">
+                <div class="dorik-icon-vi7zaosw-wrapper  ">
+                  <span class="dorik-icon dorik-icon-vi7zaosw ">
+                    <svg
+                      aria-hidden="true"
+                      focusable="false"
+                      data-prefix="far"
+                      data-icon="thumbs-up"
+                      class="svg-inline--fa fa-thumbs-up fa-w-16 fa-fw icon"
+                      role="img"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 512 512"
+                    >
+                      <path
+                        fill="currentColor"
+                        d="M466.27 286.69C475.04 271.84 480 256 480 236.85c0-44.015-37.218-85.58-85.82-85.58H357.7c4.92-12.81 8.85-28.13 8.85-46.54C366.55 31.936 328.86 0 271.28 0c-61.607 0-58.093 94.933-71.76 108.6-22.747 22.747-49.615 66.447-68.76 83.4H32c-17.673 0-32 14.327-32 32v240c0 17.673 14.327 32 32 32h64c14.893 0 27.408-10.174 30.978-23.95 44.509 1.001 75.06 39.94 177.802 39.94 7.22 0 15.22.01 22.22.01 77.117 0 111.986-39.423 112.94-95.33 13.319-18.425 20.299-43.122 17.34-66.99 9.854-18.452 13.664-40.343 8.99-62.99zm-61.75 53.83c12.56 21.13 1.26 49.41-13.94 57.57 7.7 48.78-17.608 65.9-53.12 65.9h-37.82c-71.639 0-118.029-37.82-171.64-37.82V240h10.92c28.36 0 67.98-70.89 94.54-97.46 28.36-28.36 18.91-75.63 37.82-94.54 47.27 0 47.27 32.98 47.27 56.73 0 39.17-28.36 56.72-28.36 94.54h103.99c21.11 0 37.73 18.91 37.82 37.82.09 18.9-12.82 37.81-22.27 37.81 13.489 14.555 16.371 45.236-5.21 65.62zM88 432c0 13.255-10.745 24-24 24s-24-10.745-24-24 10.745-24 24-24 24 10.745 24 24z"
+                      ></path>
+                    </svg>
+                  </span>
+                </div>
+                <div class="dorik-wrapper dorik-heading-92l6l85l-wrapper ">
+                  <h3 class="dorik-heading dorik-heading-92l6l85l ">
+                    <span>Encrypted Network</span>
+                  </h3>
+                </div>
+                <div class="dorik-text dorik-text-na6qelw4 ">
+                  Ecosystem bootstrapping learning curve lean startup
+                  disruptive. Marketing long tail disruptive agile development
+                  partner.
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="container">
+          <div class="dorik-row-elcq3d7u  flex">
+            <div class="col-lg-1/1 ">
+              <div class="column-inner dorik-column-dttukufd  1/1">
+                <div class="dorik-wrapper dorik-heading-7sy9bbxw-wrapper ">
+                  <h4 class="dorik-heading dorik-heading-7sy9bbxw ">
+                    <span>Trusted by 2,500+ Satisfied Customers </span>
+                  </h4>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="container">
+          <div class="dorik-row-71tmok6g  flex">
+            {[
+              'https://cdn.dorik.com/5e373b6c43a72a001f56dbf6/images/logo-4_oer81ikw.svg',
+              'https://cdn.dorik.com/5e373b6c43a72a001f56dbf6/images/placeholder_gci6d7wr.png',
+              'https://cdn.dorik.com/5e373b6c43a72a001f56dbf6/images/Haus_50fz3wbi.svg',
+              'https://cdn.dorik.com/5e373b6c43a72a001f56dbf6/images/nirastate_67s2bcw3.png',
+              'https://cdn.dorik.com/5e373b6c43a72a001f56dbf6/images/Haus_50fz3wbi.svg',
+              'https://cdn.dorik.com/5e373b6c43a72a001f56dbf6/images/nirastate_67s2bcw3.png',
+              'https://cdn.dorik.com/5e373b6c43a72a001f56dbf6/images/Hype_Copy_0o5fbkmj.svg',
+              'https://cdn.dorik.com/5e373b6c43a72a001f56dbf6/images/Acme_1_iv1c7xqg.svg',
+            ].map((sponsor, i) => (
+              <div
+                key={i}
+                class="col-lg-1/6 col-md-1/6 col-xs-1/3 "
+              >
+                <div class="column-inner dorik-column-glrkpfc9  1/6">
+                  <div class="dorik-wrapper dorik-image-w7rn0izl-wrapper ">
+                    <img
+                      src={sponsor}
+                      class="dorik-image-w7rn0izl "
+                    />
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div class="container">
+          <div class="dorik-row-g591xhm6  flex">
+            {[
+              'https://cdn.dorik.com/5e373b6c43a72a001f56dbf6/images/Acme_1_iv1c7xqg.svg',
+              'https://cdn.dorik.com/5e373b6c43a72a001f56dbf6/images/crofts-accountants_dlt5txsd.svg',
+              'https://cdn.dorik.com/5e373b6c43a72a001f56dbf6/images/On_White_Background_dsctfpg9.svg',
+              'https://cdn.dorik.com/5e373b6c43a72a001f56dbf6/images/logo-2_gdcam984.svg',
+              'https://cdn.dorik.com/5e373b6c43a72a001f56dbf6/images/Acme_2_i8wnk24y.svg',
+            ].map((sponsor, i) => (
+              <div
+                key={i}
+                class="col-lg-1/5 col-md-1/5 col-xs-1/3 "
+              >
+                <div class="column-inner dorik-column-glrkpfc9  1/6">
+                  <div class="dorik-wrapper dorik-image-w7rn0izl-wrapper ">
+                    <img
+                      src={sponsor}
+                      class="dorik-image-w7rn0izl "
+                    />
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
